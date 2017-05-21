6910726a31f33ae7b24d1f55fe176dc2bf335d2d
Dmitriy Ilyenkov <admin911@list.ru>
2017-05-21 19:13:05 +0500

    bash install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+proj=mysqld3kools
+basedir=$(readlink -f $(dirname $0))
+
+function usrlocal_install {
+    usrlocal=/usr/local/$proj
+    if test -d $usrlocal; then
+        echo $usrlocal already exists;
+        exit;
+    else
+        mkdir $usrlocal;
+    fi
+    for f in `ls -1 $basedir/mysql_d3*`; do
+        filename=`basename $f`;
+        cp $f $usrlocal/;
+        ln -s $usrlocal/$filename /usr/local/bin/$filename;
+    done
+}
+
+function homerc_install {
+    rcfile=$HOME/.mysql_d3.rc
+    touch $rcfile
+    if [ -n $SUDO_USER ]; then
+        chown $SUDO_USER $rcfile
+        chgrp $SUDO_USER $rcfile
+    fi
+    chmod 640 $rcfile
+}
+
+usrlocal_install
+homerc_install
+echo $proj installed
+# mkdir $usrlocal
